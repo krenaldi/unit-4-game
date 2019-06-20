851da0256cabbf8983916167e00ad0d4b453f656
Kris Renaldi <krenaldi@comcast.net>
2019-06-20 13:47:57 -0400

    Added global object array for characters

---
+++ assets/javascript/game.js
@@ -0,0 +1,44 @@
+$(document).ready(function () {
+
+    // Global object array for each character
+    var characters = {
+        "Darth Sidious": {
+            name: "Darth Sidious",
+            health: 100,
+            attack: 8,
+            image: '<img src="assets/images/darth-sidious.png" class="image">'
+        },
+        "Luke Skywalker": {
+            name: "Luke Skywalker",
+            health: 100,
+            attack: 8,
+            image: '<img src="assets/images/luke-skywalker.jpg" class="image">'
+        },
+        "Darth Maul": {
+            name: "Darth Maul",
+            health: 100,
+            attack: 8,
+            image: '<img src="assets/images/darth-maul.jpg" class="image">'
+        },
+        "Obi-Wan Kenobi": {
+            name: "Obi-Wan Kenobi",
+            health: 100,
+            attack: 8,
+            image: '<img src="assets/images/obi-wan.jpg" class="image">'
+        }
+    };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
